67ac766fe2f99a550c1774030e220593a51f2d9c
Deusdete Filho <deusdetefilho@gmail.com>
2020-07-04 08:41:28 -0300

    Updates

---
+++ precache-manifest.4a33c79dd9501e3d6faebc0ed2a3dfc1.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "17832f2be69cd8498b78",
+    "url": "/static/js/app.604f0058.chunk.js"
+  },
+  {
+    "revision": "77cafc906f42b4a876d5",
+    "url": "/static/js/runtime~app.0dd01ab9.js"
+  },
+  {
+    "revision": "05f35570c2971a5d4624",
+    "url": "/static/js/2.71a4aa0e.chunk.js"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/./fonts/Roboto_100Thin.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/./fonts/Roboto_100Thin_Italic.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/./fonts/Roboto_300Light.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/./fonts/Roboto_300Light_Italic.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/./fonts/Roboto_400Regular.ttf"
+  },
+  {
+    "revision": "a720f17aa773e493a7ebf8b08459e66c",
+    "url": "/./fonts/Roboto_400Regular_Italic.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/./fonts/Roboto_500Medium.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/./fonts/Roboto_500Medium_Italic.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/./fonts/Roboto_700Bold.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/./fonts/Roboto_700Bold_Italic.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/./fonts/Roboto_900Black.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/./fonts/Roboto_900Black_Italic.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "db726f190386fe1a1d9997e5b07b2de5",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "0610777412cc0d62a127f93d35fc7d63",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "338787eb1b74d125c2015d21eac8d8f7",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "64f5bcdad25e35bcf923a7bb538c736e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd79d181da27a1caaa642767bfdc75a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
